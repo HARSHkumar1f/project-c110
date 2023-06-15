c589d039eaa0cbd3ed6da53b1dedc57349816200
HARSHkumar1f <98209420+HARSHkumar1f@users.noreply.github.com>
2023-06-15 21:27:29 +0530

    Add files via upload

---
+++ project-110.py
@@ -0,0 +1,49 @@
+import random
+responce="y"
+response=input("enter y to rool and n to stop: ") 
+while responce=="y":
+   
+ 
+   no = random.randint(1,6)
+   if no ==1:
+    print("[- - - -]")
+    print("[       ]")
+    print("[   0   ]")
+    print("[       ]")
+    print("[- - - -]")
+    print("enter y to roll again and n to exit:")  
+   if no ==2:
+    print("[- - - -]")
+    print("[ 0      ]")
+    print("[   0   ]")
+    print("[       ]")
+    print("[- - - -]")
+    print("enter y to roll again and n to exit:")  
+   if no ==3:
+      print("[- - - -]")
+      print("[  0     ]")
+      print("[   0   ]")
+      print("[  0     ]")
+      print("[- - - -]") 
+      print("enter y to roll again and n to exit:")  
+   if no ==4:
+      print("[- - - -]")
+      print("[ 0   0  ]")
+      print("[   0   ]")
+      print("[ 0     ]")
+      print("[- - - -]") 
+      print("enter y to roll again and n to exit:")  
+   if no ==5:
+      print("[- - - -]")
+      print("[ 0    0 ]")
+      print("[   0    ]")
+      print("[ 0    0 ]")
+      print("[- - - -]") 
+      print("enter y to roll again and n to exit:")  
+   if no ==6:
+      print("[- - - -]")
+      print("[   0 0 0  ]")
+      print("[   0 0 0  ]")
+      print("[          ]")
+      print("[- - - -]")   
+      print("enter y to roll again and n to exit:")  
